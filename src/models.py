41512bcb2db0dee6e7d3af68b6493d2830f4b9bd
Rozpi <joserozpide@yahoo.es>
2022-06-09 17:51:39 +0000

    ok

--- src/models.py
+++ src/models.py
@@ -13,7 +13,7 @@ class Person(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    # name = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
